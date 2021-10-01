ce4cd6f0ddef59b1da48501b1b7cbe0b91f3966f
Yongsin0 <qwer78942506@gmail.com>
2021-10-01 11:08:51 +0800

    99-1

---
+++ homework/99.js
@@ -0,0 +1,20 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+function table99()
+{
+    
+
+}
+
+
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
+
+
